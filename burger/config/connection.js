a01037ab923dbf6654cd38e32ed4e17416f6558e
Cash <56851337+mlerey22@users.noreply.github.com>
2020-05-10 17:06:21 -0500

    connected JAWS_DB

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection =mysql.createConnection(process.env.JAWSDB_URL);
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "ThisisforDB22.",
-  database: "burgers_db"
-});
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "ThisisforDB22.",
+        database: "burgers_db"    })
+}
+ 
 
 // Make connection.
 connection.connect(function(err) {
