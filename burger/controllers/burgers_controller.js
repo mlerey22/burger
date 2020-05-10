21c749a02a434ad4d5917c7ea7dcdb62a8e8bc0b
Cash <56851337+mlerey22@users.noreply.github.com>
2020-05-10 16:55:39 -0500

    finished getting all docs started...now clean up

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -14,14 +14,14 @@ router.get("/", function(req, res) {
     });
   });
   
-  router.post("/api/burgers", function(req, res) {
+  router.post("/", function(req, res) {
     burger.create(["name", "sleepy"], [req.body.name, req.body.devoured], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
   });
   
-  router.put("/api/burgers/:id", function(req, res) {
+  router.put("/api/:id", function(req, res) {
     var condition = "id = " + req.params.id;
     
     burger.update(
